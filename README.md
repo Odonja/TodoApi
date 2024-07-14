41f19bddd1141176cb6bb06aca4570fd25ec062a
Odonja <huijsmans90@gmail.com>
2024-07-14 19:27:09 +0200

    added use of service bus to config controller and updated readme

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # oefenproject voor C#
 
-Attempting this tutorial: https://learn.microsoft.com/en-us/azure/app-service/app-service-web-tutorial-rest-api 
+Attempting this tutorial: https://learn.microsoft.com/en-us/azure/app-service/app-service-web-tutorial-rest-api
 
 At the time of making it works at https://azuretestappl.azurewebsites.net/swagger/index.html
 
@@ -18,7 +18,26 @@ Trying the Azure App Configuration
   - next look for text "An alternative approach when using the options pattern"
     to use that approach to read a configuration parameter and return that on the Config REST interface.
 
+Playing with azure service bus (part of it is in https://github.com/Odonja/animal-service-bus)
+
+- login in Azure portal en bekijk de eigenschappen van onze service bus
+- maak een console applicatie zoals hier beschreven.
+  https://github.com/Azure/azure-sdk-for-net/blob/main/sdk/servicebus/Azure.Messaging.ServiceBus/samples/Sample01_SendReceive.md
+  > connection string: ...
+  > queue: configurationqueue
+- bekijk deze list van examples, zodat je andere topics ziet die me service bus te maken heb.
+- study this article on architecture.
+  https://learn.microsoft.com/en-us/azure/architecture/example-scenario/integration/queues-events
+  From the artiacle you will understand, that in order to have a true event-driven (aka message based) service
+  that you need an Event Grid (required premium $).
+  One that is clear to you from the article, you are ready to move to the next step.
+- Use the "Send and receive a message using queues" code and integrate that into a new REST service /api/config/use-service-bus
+  This service shall use service-bus to obtain the REST configuration-response.
+
 Used sources (not complete):
+
 - https://learn.microsoft.com/en-us/azure/app-service/app-service-web-tutorial-rest-api
 - https://learn.microsoft.com/en-us/aspnet/core/tutorials/first-web-api?view=aspnetcore-8.0&tabs=visual-studio-code
 - https://learn.microsoft.com/en-us/aspnet/core/fundamentals/configuration/?view=aspnetcore-8.0
+- https://learn.microsoft.com/en-us/azure/service-bus-messaging/service-bus-samples
+-
--- dotnet-core-api/Controllers/ConfigController.cs
+++ dotnet-core-api/Controllers/ConfigController.cs
@@ -1,8 +1,8 @@
+using Azure.Messaging.ServiceBus;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Options;
+using System;
 using System.Threading.Tasks;
-using TodoApi.Models;
 
 namespace TodoApi.Controllers
 {
@@ -10,6 +10,9 @@ namespace TodoApi.Controllers
     [ApiController]
     public class ConfigController : Controller
     {
+        // set the environment variable with: setx AZURE_SERVICE_BUS_CONNECTION_STRING "<the connection string>"
+        private static readonly string? connectionString = Environment.GetEnvironmentVariable("AZURE_SERVICE_BUS_CONNECTION_STRING");
+        private static readonly string queueName = "configurationqueue";
         private const string MY_KEY = "MyKey";
         private const string POSITION_TITLE = "Position:Title";
         private const string POSITION_NAME = "Position:NamE";
@@ -29,6 +32,38 @@ namespace TodoApi.Controllers
         [HttpGet]
         public async Task<ContentResult> GetConfig()
         {
+            string configuration = ReadConfigurationFromConfigFile();
+
+            return await Task.FromResult(Content(configuration));
+        }
+
+        // GET: api/Config/use-service-bus
+        [HttpGet("use-service-bus")]
+        public async Task<ContentResult> GetConfigWithServiceBus()
+        {
+            await using var client = new ServiceBusClient(connectionString);
+            await SendConfigurationOverServiceBus(ReadConfigurationFromConfigFile(), client);
+            string configuration = await ReceiveConfigurationFromServiceBus(client);
+
+            return await Task.FromResult(Content(configuration));
+        }
+
+        private static async Task<string> ReceiveConfigurationFromServiceBus(ServiceBusClient client)
+        {
+            ServiceBusReceiver receiver = client.CreateReceiver(queueName);
+            ServiceBusReceivedMessage receivedConfigurationMessage = await receiver.ReceiveMessageAsync();
+            return receivedConfigurationMessage.Body.ToString();
+        }
+
+        private static async Task SendConfigurationOverServiceBus(string configuration, ServiceBusClient client)
+        {
+            ServiceBusSender sender = client.CreateSender(queueName);
+            ServiceBusMessage configurationMessage = new ServiceBusMessage(configuration);
+            await sender.SendMessageAsync(configurationMessage);
+        }
+
+        private string ReadConfigurationFromConfigFile()
+        {
             var myKeyValue = configuration[MY_KEY];
             var title = configuration[POSITION_TITLE];
             var name = configuration[POSITION_NAME];
@@ -44,9 +79,7 @@ namespace TodoApi.Controllers
                           $"{MOESTUIN_FRUIT_FRAMBOOS}: {moestuinFruit} \n" +
                           $"{HORSE}: {horse} \n" +
                           $"{DEFAULT_LOGLEVEL}: {defaultLogLevel}";
-
-            return await Task.FromResult(Content(content));
+            return content;
         }
-
     }
 }
